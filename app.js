240c0bc375cb20dc26f7b8877ef26f0e6c64eaf7
kwamecody <kwamecody@gmail.com>
2023-02-15 06:26:18 -0800

    initialize game variables

--- app.js
+++ app.js
@@ -8,4 +8,22 @@ form.addEventListener('submit', (event) =>{
 
     const modal =document.querySelector('.modal-wrapper');
     modal.style.display = "none";
-})
+    InitializeGame(data);
+});
+
+
+//Variables for Game
+function gameVariables(data){
+    data.choice = Number(data.choice);
+    data.board = [0,1,2,3,4,5,6,7,8];
+    data.playerOneMarker = "X";
+    data.playerTwoMarker = "O";
+    data.gameRound = 0;
+    data.gameOver = false;
+};
+
+//Initialize Game
+function InitializeGame(data){
+    gameVariables(data);
+    console.log(data);
+}
