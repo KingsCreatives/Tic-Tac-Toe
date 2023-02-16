39b271084a0dbf6b322346405fca0ff4e9698e7b
kwamecody <kwamecody@gmail.com>
2023-02-16 16:39:15 +0000

    get user data

---
+++ app.js
@@ -0,0 +1,15 @@
+//Event Listener to get user data
+const form = document.querySelector('.myForm');
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const data = Object.fromEntries(new FormData(form));
+    const modal = document.querySelector('.modal-wrapper');
+    modal.style.display = "none";
+    console.log(data);
+});
+
+//Initialize Game
+
+
+//set win condition
